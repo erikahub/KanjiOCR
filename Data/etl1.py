0acba9edae9f626ce061b9c2b8643e4ab70dda66
Bjoern Lewe <bjoern.lewe@gmx.de>
2019-12-28 18:27:06 +0100

    Revert "create folder if it does not exists"
    This reverts commit 18a7b4ffd8f42c0e0ee7027dc8ce76c7b0c6dea2.

--- Data/etl1.py
+++ Data/etl1.py
@@ -2,19 +2,14 @@
 
 import struct
 from PIL import Image, ImageEnhance
-from os import listdir, mkdir
-from os.path import isfile, join, exists
-folder = 'DatasetETLCDB/ETL1'
+from os import listdir
+from os.path import isfile, join
+folder = 'Data/DatasetETLCDB/ETL1'
 allfiles = [f for f in listdir(folder) if isfile(join(folder, f))]
 filename = folder + '/ETL1C_07'
 # skip = 100
 with open(filename, 'rb') as f:
     # f.seek(skip * 2052)
-    fileFolder = folder + 'PNG/' + filename.split('/')[-1] + '/'
-    if not exists(folder + 'PNG/'):
-        mkdir(folder + 'PNG/')
-        if not exists(fileFolder):
-            mkdir(fileFolder)
     while f.readable():
         s = f.read(2052)
         if s == None or len(s) < 2052:
