6ad39b62b1803e882698098607afa5f14672a6ad
Erika <huettebraeucker.robin@gmail.com>
2019-12-28 17:31:43 +0100

    Update etl1.py
    reads all data from a file now and saves pngs

--- Data/etl1.py
+++ Data/etl1.py
@@ -1,16 +1,25 @@
+"""This code is largely imported from the specification sites of http://etlcdb.db.aist.go.jp/ for the corresponding dataset"""
+
 import struct
 from PIL import Image, ImageEnhance
-
-filename = 'ETL1/ETL1C_01'
-skip = 100
+from os import listdir
+from os.path import isfile, join
+folder = 'Data/DatasetETLCDB/ETL1'
+allfiles = [f for f in listdir(folder) if isfile(join(folder, f))]
+filename = folder + '/ETL1C_07'
+# skip = 100
 with open(filename, 'rb') as f:
-    f.seek(skip * 2052)
-    s = f.read(2052)
-    r = struct.unpack('>H2sH6BI4H4B4x2016s4x', s)
-    iF = Image.frombytes('F', (64, 63), r[18], 'bit', 4)
-    iP = iF.convert('P')
-    fn = "{:1d}{:4d}{:2x}.png".format(r[0], r[2], r[3])
-#    iP.save(fn, 'PNG', bits=4)
-    enhancer = ImageEnhance.Brightness(iP)
-    iE = enhancer.enhance(16)
-    iE.save(fn, 'PNG')
+    # f.seek(skip * 2052)
+    while f.readable():
+        s = f.read(2052)
+        if s == None or len(s) < 2052:
+            break
+        r = struct.unpack('>H2sH6BI4H4B4x2016s4x', s)
+        iF = Image.frombytes('F', (64, 63), r[18], 'bit', 4)
+        # iP = iF.convert('P')
+        iP = iF.convert('L')
+        fn = "{:1d}{:4d}{:2x}.png".format(r[0], r[2], r[3])
+        # iP.save('Data/' + fn, 'PNG', bits=4)
+        enhancer = ImageEnhance.Brightness(iP)
+        iE = enhancer.enhance(16)
+        iE.save(folder + 'PNG/' + filename.split('/')[-1] + '/' + fn, 'PNG')
