427d5d1ee895b8636afbff02f1c7dfc943c65866
Erika <huettebraeucker.robin@gmail.com>
2020-02-07 14:00:39 +0100

    Renamed etl1 script for images

--- Data/etl1images.py
+++ Data/etl1images.py
@@ -5,7 +5,7 @@ import struct
 from PIL import Image, ImageEnhance
 from os import listdir
 from os.path import isfile, join
-folder = 'Data/DatasetETLCDB/ETL1'
+folder = 'Data/DatasetETLCDB/ETL1SPLIT/'
 allfiles = [f for f in listdir(folder) if isfile(join(folder, f))]
 filename = folder + '/ETL1C_07'
 # skip = 100
@@ -18,12 +18,13 @@ with open(filename, 'rb') as f:
         #19 records, omitted records (x option) with undefined data
         r = struct.unpack('>H2sH6BI4H4B4x2016s4x', s)
         iF = Image.frombytes('F', (64, 63), r[18], 'bit', 4)
+        #P maps to 8bit pixels in color, L maps to 8bit pixels black and white
         # iP = iF.convert('P')
         iP = iF.convert('L')
-        #r[0]:=Data Index, r[2]:=Sheet Index, r[3]:=Character Code (JIS X0201)
-        fn = "{:1d}_{:4d}_{:2x}.png".format(r[0], r[2], r[3])
+        # r[2]:=Sheet Index (characters A and B may have the same index, but no A has the same index as another one), 
+        # r[3]:=Character Code (JIS X0201)
+        fn = "{:4d}_{:2x}.png".format(r[2], r[3])
         # iP.save('Data/' + fn, 'PNG', bits=4)
         enhancer = ImageEnhance.Brightness(iP)
         iE = enhancer.enhance(16)
         iE.save(folder + 'PNG/' + filename.split('/')[-1] + '/' + fn, 'PNG')
-        # 0x2A22  0x3042  # HIRAGANA LETTER A
