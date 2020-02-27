502c74a4d43514868ab0b90d32d880de106f8c39
Erika <huettebraeucker.robin@gmail.com>
2020-02-27 13:31:59 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -17,17 +17,24 @@ import dataconverter
 dc = dataconverter.DataConverter()
 dc.load()
 #ds stands for dataset
-ds_joint = tf.data.Dataset.from_tensor_slices((dc.features, dc.labels)) #x: features, y: labels
 
+# for i in range(len(dc.labels)):
+    # dc.labels[i]+=f'_{i}'
+ds_joint = tf.data.Dataset.from_tensor_slices((dc.features, dc.labels)) #x: features, y: labels
 def printSome(dataset, number: int):
     c = 0
-    for example in ds_joint:
+    for example in dataset:
         c+=1
         print('x:', example[0].numpy()[:10], ' y:', example[1].numpy())
         if c >= number:
             break
 
-tf.random.set_seed(1)
-ds = ds_joint.shuffle(buffer_size=100)
+tf.random.set_seed(182)
+ds = ds_joint.shuffle(buffer_size=1000, reshuffle_each_iteration=False)
+# printSome(ds, 4)
 
-printSome(ds, 4)
+train_size, ds_size = 0.7, len(dc.labels)
+take_size_train = train_size * ds_size
+take_size_test = ds_size - take_size_train
+
+ds_train = ds.take(take_size_train), ds.skip(take_size_train).take(take_size_test)
