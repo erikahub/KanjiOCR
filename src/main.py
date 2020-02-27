ec72b405f6c0eaf6e478de2448f85446de148444
Erika <huettebraeucker.robin@gmail.com>
2020-02-27 12:55:30 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -10,11 +10,24 @@
 #     data_paths += listdir(paths.join(data_path, 'train', folder))
 # del tmp
 # img = imageio.imread(paths.join(data_path, 'train', '0', '0001_0.png'))
-# import tensorflow as tf
 
+import tensorflow as tf
 import dataconverter
+
 dc = dataconverter.DataConverter()
 dc.load()
-dc.lables
-ds_x = tf.data.Dataset.from_tensor_slices(img)
-print(ds_x)
+#ds stands for dataset
+ds_joint = tf.data.Dataset.from_tensor_slices((dc.features, dc.labels)) #x: features, y: labels
+
+def printSome(dataset, number: int):
+    c = 0
+    for example in ds_joint:
+        c+=1
+        print('x:', example[0].numpy()[:10], ' y:', example[1].numpy())
+        if c >= number:
+            break
+
+tf.random.set_seed(1)
+ds = ds_joint.shuffle(buffer_size=100)
+
+printSome(ds, 4)
