c266838d18aead97f64aba1a8e0fc091faa89712
Erika <huettebraeucker.robin@gmail.com>
2020-02-27 12:36:38 +0100

    Update paths.py

--- src/paths.py
+++ src/paths.py
@@ -1,9 +1,12 @@
 """This module's sole purpose is to get the path of this module and the root folder directory in order to be able to work with the project structure
 author: rohue"""
-from os.path import dirname, abspath, split
+from os.path import dirname, abspath, split, join
 
 def getPath():
     return dirname(abspath(__file__))
     
 def getRootPath():
     return split(dirname(abspath(__file__)))[0]
+
+def getDBPath():
+    return join(getRootPath(), 'Data','DatasetETLCDB')
