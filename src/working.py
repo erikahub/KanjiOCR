646a7bb49d4c2689a889a43165dc80ffeeb9cd49
Erika <huettebraeucker.robin@gmail.com>
2020-03-02 13:43:37 +0100

    Update working.py
    Workin on accuracy...

--- src/working.py
+++ src/working.py
@@ -5,12 +5,12 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Activation, Dropout, Conv2D, MaxPooling2D
 import paths
 from os import listdir
-from time import time, asctime, struct_time
+from time import time
 
 ## In[]
 start = time()
 
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 IMG_HEIGHT = 63
 IMG_WIDTH = 64
 IMG_CHANNELS = 1
@@ -40,13 +40,13 @@ print('Took: ',time() - start, 'seconds')
 model = Sequential()
 model.add(Conv2D(32,(5,5), padding='same', activation='relu', 
                                  input_shape=(IMG_HEIGHT,IMG_WIDTH,IMG_CHANNELS)))
-#model.add(MaxPooling2D((2, 2)))
+model.add(MaxPooling2D((3, 3)))
 model.add(Conv2D(64, (5, 5), padding='same', activation='relu'))
-#model.add(MaxPooling2D((2, 2)))
+model.add(MaxPooling2D((3, 3)))
 model.add(Flatten())
 model.add(Dense(128, activation='softmax'))
-model.add(Dense(1))
-
+model.add(Dense(11)) #this is supposed to be the number of labels.
+#TODO try changing the image data. Maybe remove enhancement
 
 #sparse_categorical_crossentropy results in the model only allowing the number of labels input into Dense -1.
 #one-hot encoding is required with categorical_crossentropy
@@ -56,11 +56,16 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 time
-start = asctime(time())
+start = time()
 
-model.fit(train_data_gen,
-          epochs=1, #TODO change this back to higher numbers to try achieving higher accuracy
+fit_history = model.fit(train_data_gen,
+          epochs=10, #TODO change this back to higher numbers to try achieving higher accuracy
           verbose=1)
-end=asctime(time())
-print(f'Start time: {ascitime(start)}, End time: {ascitime(end)}, took {asctime(end-start)}')
+end=time()
+print(f'Training took {(end-start)//60:.0}min {int(end-start)%60}seconds')
+
+test_loss, test_acc = model.evaluate(test_data_gen, 
+                verbose=1)
+
+print(f'Test loss: {test_loss}, test accuracy: {test_acc}')
 # model.fit(x=x_train, y=y_train, epochs=1, steps_per_epoch=1, verbose=1)
